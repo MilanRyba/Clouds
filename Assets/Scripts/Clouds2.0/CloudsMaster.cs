ea6672efb6300cd089e1dc843f35f79b7c3791a8
MilanRyba <95483260+MilanRyba@users.noreply.github.com>
2025-11-12 21:18:49 +0100

    Started raymarching

--- Assets/Scripts/Clouds2.0/CloudsMaster.cs
+++ Assets/Scripts/Clouds2.0/CloudsMaster.cs
@@ -1,10 +1,27 @@
 using Helpers;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 
 [ExecuteInEditMode, ImageEffectAllowedInSceneView]
 public class CloudsMaster : MonoBehaviour
 {
-	[SerializeField] Texture3D m_ShapeTexture;
+	[Header("Noise")]
+
+	[Range(0, 1)] public int PerlinMethod = 0;
+	[Range(0, 1)] public int WorleyMethod = 0;
+	[Range(0, 100)]     public int Seed = 0;
+	[Range(1, 10)]      public int ShapeNoiseFrequency = 3;
+	[Range(32, 256)]    public int ShapeNoiseResolution = 128;
+	// [Range(0.1f, 5.0f)] public float ShapeNoiseScale = 0.3f;
+
+	[Header("Clouds")]
+	public Transform Container;
+
+	[Range(0.01f, 10.0f)]   public float RayStepSize = 0.15f;
+	[Range(0.0f, 1.0f)]     public float GlobalDensity = 0.1f;
+	[Range(0.01f, 0.0005f)] public float GlobalScale = 0.001f;
+	[Range(0.1f, 5.0f)]     public float ShapeNoiseScale = 0.3f;
+	[Range(0.0f, 1.0f)]     public float Coverage = 0.9f;
 
 	[Header("Debug")]
 
@@ -28,47 +45,48 @@ public class CloudsMaster : MonoBehaviour
 		}
 	}
 
-	[Header("Noise")]
-
-	[Range(0, 1)] public int PerlinMethod = 0;
-	[Range(0, 1)] public int WorleyMethod = 0;
-	[Range(0, 100)]     public int Seed = 0;
-	[Range(1, 10)]      public int ShapeNoiseFrequency = 3;
-	[Range(32, 256)]    public int ShapeNoiseResolution = 128;
-	// [Range(0.1f, 5.0f)] public float ShapeNoiseScale = 0.3f;
-
 	[Header("Shaders")]
 
+	[SerializeField] ComputeShader m_Clouds;
 	[SerializeField] ComputeShader m_CloudShapeNoise;
-	[SerializeField] ComputeShader m_CloudDebug;
 
+	private RenderTexture m_IntermediateTexture;
 	private RenderTexture m_ShapeNoise;
-
-	private RenderTexture m_DebugTexture;
 
 	private void OnRenderImage(RenderTexture source, RenderTexture destination)
 	{
-		if (EnableDebug)
-		{
-			// Debug Pass
-			TextureHelper.CreateTexture2D(ref m_DebugTexture, source.width, source.height, "DebugTexture");
+		TextureHelper.CreateTexture2D(ref m_IntermediateTexture, source.width, source.height, "Clouds Intermediate Texture");
 
-			m_CloudDebug.SetFloat("TextureSlice", TextureSlice);
-			m_CloudDebug.SetVector("ViewportDimensions", new Vector2(source.width, source.height));
-			m_CloudDebug.SetVector("ViewportDimensionsInv", new Vector2(1.0f / source.width, 1.0f / source.height));
-			m_CloudDebug.SetVector("ChannelMask", ChannelMask);
+		m_Clouds.SetVector("ViewportDimensions", new Vector2(source.width, source.height));
+		m_Clouds.SetVector("ViewportDimensionsInv", new Vector2(1.0f / source.width, 1.0f / source.height));
+		m_Clouds.SetVector("CameraPosition", transform.position);
 
-			m_CloudDebug.SetTexture(0, "ShapeTexture", m_ShapeNoise);
-			m_CloudDebug.SetTexture(0, "SourceTexture", source);
-			m_CloudDebug.SetTexture(0, "Output", m_DebugTexture);
+		Camera camera = GetComponent<Camera>();
+		m_Clouds.SetMatrix("CameraToWorld", camera.cameraToWorldMatrix);
+		m_Clouds.SetMatrix("CameraInverseProjection", camera.projectionMatrix.inverse);
 
+		m_Clouds.SetVector("BoundsMin", Container.position - Container.localScale / 2);
+		m_Clouds.SetVector("BoundsMax", Container.position + Container.localScale / 2);
 
-			GraphicsHelper.Dispatch(m_CloudDebug, source.width, source.height);
+		m_Clouds.SetFloat("RayStepSize", RayStepSize);
+		m_Clouds.SetFloat("GlobalDensity", GlobalDensity);
+		m_Clouds.SetFloat("GlobalScale", GlobalScale);
+		m_Clouds.SetFloat("ShapeNoiseScale", ShapeNoiseScale);
+		m_Clouds.SetFloat("Coverage", Coverage);
+		
+		m_Clouds.SetTexture(0, "ShapeTexture", m_ShapeNoise);
+		m_Clouds.SetTexture(0, "SceneTexture", source);
+		// m_Clouds.SetTexture(0, "DepthTexture", Shader.GetGlobalTexture("_CameraDepthTexture"));
+		m_Clouds.SetTexture(0, "Output", m_IntermediateTexture);
 
-			Graphics.Blit(m_DebugTexture, destination);
-		}
-		else
-			Graphics.Blit(source, destination);
+		// Debug parameters
+		// m_Clouds.SetBool("Debug", EnableDebug);
+		// m_Clouds.SetFloat("TextureSlice", TextureSlice);
+		// m_Clouds.SetVector("ChannelMask", ChannelMask);
+
+		GraphicsHelper.Dispatch(m_Clouds, source.width, source.height);
+
+		Graphics.Blit(m_IntermediateTexture, destination);
 	}
 
 	private void OnValidate()
